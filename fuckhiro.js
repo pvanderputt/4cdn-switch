118f0686dedad96f16f003ee7371344b51d81c92
pvanderputt <patrickvanderputt@gmail.com>
2016-12-27 20:54:21 -0500

    added check if our href exists

--- fuckhiro.js
+++ fuckhiro.js
@@ -1,6 +1,8 @@
 var FH_replaceImages = document.getElementsByClassName("fileThumb");
 var FH_iterator = 0;
 for(; FH_iterator < FH_replaceImages.length; FH_iterator++){
-    FH_replaceImages[FH_iterator].href = FH_replaceImages[FH_iterator].href.replace(/is.4chan/, "i.4cdn");
+    if(FH_replaceImages[FH_iterator].href){
+        FH_replaceImages[FH_iterator].href = FH_replaceImages[FH_iterator].href.replace(/is.4chan/, "i.4cdn");
+    }
 }
 console.log(FH_iterator + " pieces of bullshit replaced.");
