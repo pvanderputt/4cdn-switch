e0ea9c1fc0b835c09df6cfcef0393827437de627
p0wer0n <p0wer0n@users.noreply.github.com>
2017-01-01 20:13:11 -0500

    Added support for nuking filename redirects
    I haven't compiled this yet

--- fuckhiro.js
+++ fuckhiro.js
@@ -1,8 +1,20 @@
-var FH_replaceImages = document.getElementsByClassName("fileThumb");
-var FH_iterator = 0;
-for(; FH_iterator < FH_replaceImages.length; FH_iterator++){
-    if(FH_replaceImages[FH_iterator].href){
-        FH_replaceImages[FH_iterator].href = FH_replaceImages[FH_iterator].href.replace(/is.4chan/, "i.4cdn");
+var FH_replaceImageThumbs = document.getElementsByClassName("fileThumb");
+var FH_replaceImageText = document.getElementsByClassName("fileText");
+//Replaces links from the thumbnail
+var FH_thumbIterator = 0;
+for(; FH_thumbIterator < FH_replaceImageThumbs.length; FH_thumbIterator++){
+    if(FH_replaceImageThumbs[FH_thumbIterator].href){
+        FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is.4chan/, "i.4cdn");
     }
 }
-console.log(FH_iterator + " pieces of bullshit replaced.");
+//Replaces links from the filename text
+var FH_textIterator = 0;
+for(; FH_textIterator < FH_replaceImageText.length; FH_textIterator++){
+    if(FH_replaceImageText[FH_textIterator].href){
+        FH_replaceImageText[FH_textIterator].href = FH_replaceImageText[FH_textIterator].href.replace(/is.4chan/, "i.4cdn");
+    }
+}
+
+
+console.log(FH_thumbIterator + " pieces of thumbnail bullshit replaced.");
+console.log(FH_textIterator + " pieces of filename bullshit replaced.");
