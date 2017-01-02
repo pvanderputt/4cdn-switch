c7b8f1a337d765031635019429ce9c6cc18e6469
p0wer0n <p0wer0n@users.noreply.github.com>
2017-01-01 20:44:50 -0500

    Actually fixed the filenames
    This is tested and working.

--- fuckhiro.js
+++ fuckhiro.js
@@ -1,20 +1,19 @@
 var FH_replaceImageThumbs = document.getElementsByClassName("fileThumb");
 var FH_replaceImageText = document.getElementsByClassName("fileText");
-//Replaces links from the thumbnail
+//Replaces the slow links in the thumbnail
 var FH_thumbIterator = 0;
 for(; FH_thumbIterator < FH_replaceImageThumbs.length; FH_thumbIterator++){
     if(FH_replaceImageThumbs[FH_thumbIterator].href){
         FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is.4chan/, "i.4cdn");
     }
 }
-//Replaces links from the filename text
+//Replaces the slow links the filename text
 var FH_textIterator = 0;
 for(; FH_textIterator < FH_replaceImageText.length; FH_textIterator++){
-    if(FH_replaceImageText[FH_textIterator].href){
-        FH_replaceImageText[FH_textIterator].href = FH_replaceImageText[FH_textIterator].href.replace(/is.4chan/, "i.4cdn");
+    if(FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href){
+        var imageText = FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href.replace(/is.4chan/, "i.4cdn");
+        FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].setAttribute('href', imageText);
     }
 }
-
-
 console.log(FH_thumbIterator + " pieces of thumbnail bullshit replaced.");
 console.log(FH_textIterator + " pieces of filename bullshit replaced.");
