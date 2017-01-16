f39a552fcd06e1db00cf198f3b2a760113e428e7
p0wer0n <p0wer0n@users.noreply.github.com>
2017-01-16 17:45:26 -0500

    Make the extension always on

--- fuckhiro.js
+++ fuckhiro.js
@@ -1,20 +1,28 @@
 var FH_replaceImageThumbs = document.getElementsByClassName("fileThumb");
 var FH_replaceImageText = document.getElementsByClassName("fileText");
+while(true){
+    little_boy();
+    fat_man();
+}
 //Replaces the slow links in the thumbnail
-var FH_thumbIterator = 0;
-for(; FH_thumbIterator < FH_replaceImageThumbs.length; FH_thumbIterator++){
-    if(FH_replaceImageThumbs[FH_thumbIterator].href){
-        FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is.4chan/, "i.4cdn");
-        FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is2.4chan/, "i.4cdn");
+function little_boy() {//https://en.wikipedia.org/wiki/Hiroshima
+    var FH_thumbIterator = 0;
+    for(; FH_thumbIterator < FH_replaceImageThumbs.length; FH_thumbIterator++){
+        if(FH_replaceImageThumbs[FH_thumbIterator].href){
+            FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is.4chan/, "i.4cdn");
+            FH_replaceImageThumbs[FH_thumbIterator].href = FH_replaceImageThumbs[FH_thumbIterator].href.replace(/is2.4chan/, "i.4cdn");
+        }
     }
 }
 //Replaces the slow links the filename text
-var FH_textIterator = 0;
-for(; FH_textIterator < FH_replaceImageText.length; FH_textIterator++){
-    if(FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href){
-        var imageText = FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href.replace(/is.4chan/, "i.4cdn");
-        var imageText = FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href.replace(/is2.4chan/, "i.4cdn");
-        FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].setAttribute('href', imageText);
+function fat_man() {//https://en.wikipedia.org/wiki/Nagasaki
+    var FH_textIterator = 0;
+    for(; FH_textIterator < FH_replaceImageText.length; FH_textIterator++){
+        if(FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href){
+            var imageText = FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href.replace(/is.4chan/, "i.4cdn");
+            var imageText = FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].href.replace(/is2.4chan/, "i.4cdn");
+            FH_replaceImageText[FH_textIterator].getElementsByTagName('a')[0].setAttribute('href', imageText);
+        }
     }
 }
 console.log(FH_thumbIterator + " pieces of thumbnail bullshit replaced.");
